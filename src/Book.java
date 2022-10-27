ae4c08dcac77b156ba148763fb6aa0878301cedd
Igoryok8 <energymag@mail.ru>
2022-10-28 01:42:45 +0300

    Созданы два конструктора BooK и Author.

---
+++ src/Book.java
@@ -0,0 +1,12 @@
+public class Book {
+    String nameBook;
+    String authorName;
+    int yearIssue;
+
+    public Book(String nameBook, String authorName, int yearIssue) {
+        this.nameBook = nameBook;
+        this.authorName = authorName;
+        this.yearIssue = yearIssue;
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Домашнее задание 13");
+        System.out.println();
+        System.out.println("1.");
+
+
+        Book book = new Book("Мастер и Маргарита","Михаил Булгаков",1966);
+        System.out.println("book.nameBook = " + book.nameBook);
+        System.out.println("book.authorName = " + book.authorName);
+        System.out.println("book.yearIssue = " + book.yearIssue);
+        Book book1 = new Book("Преступление и наказание","Фёдор Достоевский",1866);
+        System.out.println();
+        System.out.println("2.");
+        Author author = new Author("Михаил","Булгаков");
+        System.out.println("author.firstName = " + author.firstName);
+        System.out.println("author.lastName = " + author.lastName);
+        Author author1 = new Author("Фёдор","Достоевский");
     }
+
 }
